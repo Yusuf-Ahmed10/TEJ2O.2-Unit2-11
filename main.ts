8b5cd133c80792ada6d5c75264c64f65c7095fb0
Yusuf-Ahmed10 <yusuf.ahmed1@stu.ocsb.ca>
2026-04-08 13:06:33 -0400

    changed and committed

--- main.ts
+++ main.ts
@@ -5,4 +5,38 @@
  * This program ...
 */
 
+// variable
+let number1: number = randint(0,99)
+
+let number2: number = randint(0,99)
+
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+
+// show number 1
+input.onButtonPressed(Button.A, function () {
+    basic.clearScreen()
+    basic.showString("#1: " + number1)
+    basic.showIcon(IconNames.Happy)
+})
+
+// show number 2
+input.onButtonPressed(Button.B, function () {
+    basic.clearScreen()
+    basic.showString("#2: " + number2)
+    basic.showIcon(IconNames.Happy)
+})
+
+// show which is greater and which is less
+input.onGesture(Gesture.Shake, function () {
+    if (number1 < number2) {
+      basic.clearScreen()
+      basic.showString(number1 + "<" + number2)
+      basic.showIcon(IconNames.Sad)
+    } else  {
+    basic.clearScreen()
+    basic.showString(number1 + ">" + number2)
+    basic.showIcon(IconNames.Sad)
+    } 
+})
 basic.showString('Hello, World!')
